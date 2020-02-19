9fe13e7b56c9afbfba1dfcc1bd53f4789b8ad4ba
seven.zhong <seven.zhong@vipshop.com>
2020-02-19 15:23:36 +0800

    add ccc mini code

--- ccc.js
+++ ccc.js
@@ -4,6 +4,7 @@ const socket = require('./utils/rtcSocket').callSocket;
 const log = require('./utils/log');
 const { seatLog } = require('./utils/request');
 const app = getApp();
+let heartbeatTimer = null;
 let callTimer = null;
 Page({
   data: {
@@ -19,6 +20,9 @@ Page({
     callPrivateMapKey: null,
     callStatus: null,
 
+    // socket 连接
+    relogin: false,
+    socketFreq: 0,
     // IM 转视频
     groupId: '',
     ivrFinish: false,
@@ -35,7 +39,7 @@ Page({
       userInfo: app.globalData.userInfo,
       servingNum: app.globalData.servingNum,
       imUserSig: app.globalData.imUserSig
-    }, () => { socket.open() })
+    }, () => { this.openSocket() })
   },
   onReady: function() {},
   onShow: function() {},
@@ -44,7 +48,125 @@ Page({
   },
   onUnload: function () {
     socket.close()
+    clearInterval(heartbeatTimer)
     this.onHandleExitRoomEvent()
+  },
+  openSocket() {
+    console.log('socket create')
+    const { socketFreq } = this.data
+    if(socketFreq === 2) { //  连续两次重连 停止5秒
+      console.log('socket open timeout')
+      log.error('socket open count:', socketFreq)
+      setTimeout(() => {
+        this.setData({
+          socketFreq: 0
+        }, () => {
+          this.openSocket()
+        })
+      }, 3000)
+      return
+    }
+    this.setData({
+      socketFreq: socketFreq + 1
+    })
+    socket.createSocket()
+      .then(res => {
+        console.log('socket create success', res)
+        this.initSocket()
+      })
+      .catch(err => {
+        seatLog(err, 'socket create fail')
+        console.log('socket create fail', err)
+      })
+  },
+  initSocket() {
+    socket.on('open', header => {
+      console.log('socket open: ', header)
+      this.socketLogin()
+    })
+    socket.on('error', error => {
+      console.error('socket error: ', error)
+      seatLog(error, 'socket error')
+      this.openSocket()
+    })
+    socket.on('message', event => {
+      console.log('socket event: ', event)
+      const data = JSON.parse(event.data)
+      this.onHandleSocketMessage(data)
+    })
+  },
+  heartbeat() {
+    this.setData({
+      socketFreq: 0  // 登录成功清零
+    })
+    if(heartbeatTimer) clearInterval(heartbeatTimer)
+    heartbeatTimer = setInterval(() => {
+      this.sendHeartbeat('heartbeat')
+    }, 2000)
+  },
+  sendHeartbeat(type) {
+    const { userId, sdkAppId } = this.data
+    return socket.request(type, {
+      data: {
+        staff: {
+          userId,
+          sdkAppId
+        },
+        command: 'heartbeat'
+      }
+    }).then(e => {
+      if (e.errorCode !== '0') {
+        console.error('heartbeat error', e)
+        log.error('heartbeat error', err)
+      }
+      return e
+    }).catch(err => {
+      log.error('heartbeat error', err)
+      this.openSocket()  // 错误重连
+    })
+  },
+  socketLogin() {
+    const { userId, sdkAppId } = this.data
+    const data = {
+      staff: {
+        userId,
+        sdkAppId
+      },
+      sessionKey: '',
+      command: this.data.relogin ? 'relogin' : 'login'
+    }
+    socket.request('login', { data })
+      .then(event => {
+        this.setData({
+          relogin: true
+        })
+        if (event.errorCode === '-9') {
+          wx.showToast({
+            title: '初始化失败！',
+            success: () => {
+              wx.navigateBack()
+            },
+          })
+        } else if(event.errorCode === '-10') {
+          wx.showToast({
+            title: '账号重复登录',
+            icon: 'none'
+          })
+        } else {
+          this.sendHeartbeat('firstHeartbeat').then(msg => {
+            console.log('first heartbeat', msg)
+            this.heartbeat()
+          })
+        }
+      }).catch(e => {
+      console.error('socket login error: ', e)
+      wx.showToast({
+        title: '初始化失败！',
+        success: () => {
+          // wx.navigateBack()
+        },
+      })
+    })
   },
   onHandleSocketMessage(data) {
     data.event && console.log('收到事件:', data.event)
@@ -118,6 +240,7 @@ Page({
       }
     });
     seatLog(data) // 挂断上报
+
     this.selectComponent('#call').endCall()
     clearInterval(callTimer)
   },
--- utils/rtcSocket.js
+++ utils/rtcSocket.js
@@ -1,8 +1,6 @@
 const NonceEvent = require('nonceEvent')
 const md5 = require('md5')
 const app = getApp()
-let heartbeatTimer = null
-let heartbeatErrorCount = 0
 class RtcSocket {
   constructor(url) {
     this.socket = null
@@ -10,145 +8,50 @@ class RtcSocket {
     this.errCount = 0
   }
 
-  open() {
-    this._createSocket()
-      .then(res => {
-        console.log('socket create success', res)
-        this._initSocketEvent()
-      })
-      .catch(err => {
-        seatLog(err, 'socket create fail')
-        console.log('socket create fail', err)
-        wx.showToast({
-          title: '初始化失败！请联系管理员 ' + err,
-          icon: 'none',
-          success: () => {
-            setTimeout(() => wx.navigateBack(), 2000)
-          }
-        })
-      })
-  }
-
-  _createSocket() {
-    const currentPages = getCurrentPages()
-    const cccPage = currentPages[currentPages.length - 1]
-    const self = this
+  createSocket() {
     return new Promise((resolve, reject) => {
-      console.log('socket connect')
-      if (cccPage.route !== 'pages/ccc/ccc') reject('页面错误')
-      if (!app.globalData.wsHost) reject('socket地址不存在')
-
       this.socket = wx.connectSocket({
         url: this.url,
         success(res) {
-          console.log('socket connect success')
           resolve(res)
         },
         fail(err) {
-          console.error('socket connect fail', err)
           reject(err)
         }
       })
     })
   }
 
-  _login(relogin = false) {
-    const { openId, sdkAppId } = app.globalData
-    heartbeatErrorCount = 0
-    const data = {
-      staff: {
-        userId: openId,
-        sdkAppId
-      },
-      sessionKey: '',
-      command: relogin ? 'relogin' : 'login'
-    }
-    this.request('login', { data })
-      .then(event => {
-        if (event.errorCode === '-9') this.goBackWithToast('初始化失败: socket login fail')
-        else if (event.errorCode === '-10') this.goBackWithToast('账号重复登录')
-        else if (event.errorCode !== '0') this.goBackWithToast('登录失败，请联系管理员 code:' + event.errorCode)
-        else {
-          this._sendHeartbeat('firstHeartbeat').then(msg => {
-            console.log('socket first heartbeat', msg)
-            this._heartbeat()
-          })
-        }
-      }).catch(e => {
-        console.error('socket login error: ', e)
-        this.goBackWithToast('初始化失败: socket login fail')
-      })
-  }
-
-  // 心跳请求
-  _sendHeartbeat(type) {
-    const { openId, sdkAppId } = app.globalData
-    return this.request(type, {
-      data: {
-        staff: {
-          userId: openId,
-          sdkAppId
-        },
-        command: 'heartbeat'
-      }
-    }).then(e => {
-      if (e.errorCode !== '0') {
-        console.error('heartbeat error', e)
-        log.error('heartbeat error', err)
-        heartbeatErrorCount++
-
-        // 心跳连续错误3次重新登录
-        if (heartbeatErrorCount > 2) {
-          this._login(true)
-        }
-      }
-      heartbeatErrorCount = 0
-      return e
-    }).catch(err => {
-      this._login(true)  // 错误重登录
-    })
-  }
-
-  _heartbeat() {
-    if (heartbeatTimer) clearInterval(heartbeatTimer)
-    heartbeatTimer = setInterval(() => {
-      this._sendHeartbeat('heartbeat')
-    }, 2000)
-  }
-
-  _initSocketEvent() {
-    const currentPages = getCurrentPages()
-    const cccPage = currentPages[currentPages.length - 1]
-    const self = this
-    console.log('socket pages', cccPage)
-    this.socket.onOpen(event => {
-      this._login()
-    })
-    this.socket.onClose(event => {
-      clearInterval(heartbeatTimer)
-      console.log('socket close', event)
-    })
-    this.socket.onError(event => {
-      console.error('socket error', event)
-      this._login(true)
-    })
-    this.socket.onMessage(event => {
-      console.log('socket receive message: ', event)
-      const data = JSON.parse(event.data)
-      if (data.command = 's2c') {
-        this.send({
-          data: {
-            'errorCode': data.error || '0',
-            'nonce': data.nonce,
-            'msg': data.msg || 'message'
+  on(event, cb) {
+    switch (event) {
+      case 'close':
+        this.socket.onClose(cb)
+        break
+      case 'error':
+        this.socket.onError(cb)
+        break
+      case 'message':
+        this.socket.onMessage((event) => {
+          const data = JSON.parse(event.data)
+          if(data.command = 's2c') {
+            this.send({
+              data: {
+                'errorCode': data.error || '0',
+                'nonce': data.nonce,
+                'msg': data.msg || 'message'
+              }
+            })
           }
+          NonceEvent.emit(data.nonce, data)
+          cb && cb(event)
         })
-      }
-      NonceEvent.emit(data.nonce, data)
-      cccPage.onHandleSocketMessage && cccPage.onHandleSocketMessage(data)
-      // 没有心跳重连
-      if (data.event === '100') this._login(true)
-    })
+        break
+      case 'open':
+        this.socket.onOpen(cb)
+        break
+      default:
+        break
+    }
   }
 
   send(option, errorCb) {
@@ -159,7 +62,7 @@ class RtcSocket {
   }
 
   close() {
-    this.socket.close();
+    this.socket.close()
   }
 
   request(type, option) {
@@ -168,16 +71,6 @@ class RtcSocket {
       option.data.nonce = nonce
       this.send(option, err => reject(err))
       NonceEvent.on(nonce, e => resolve(e))
-    })
-  }
-
-  goBackWithToast(title) {
-    wx.showToast({
-      title,
-      icon: 'none',
-      success: () => {
-        setTimeout(() => wx.navigateBack(), 2000)
-      }
     })
   }
 }
